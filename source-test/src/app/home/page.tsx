45d5998243eda00a357563a0c643b9fad32e3c01
ngphuc2808 <ngphuc.2808@gmail.com>
2024-05-23 15:16:43 +0700

    Update source fix flict UI with Antd

---
+++ source-test/src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default HomePage;
